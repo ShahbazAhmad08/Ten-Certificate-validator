9c0703b4e73af2cd2093d00addda3a687c439e67
azeemsln <mdazeem2498@gmail.com>
2025-11-06 17:47:29 +0530

    added some file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ValidatePage from "./pages/ValidatePage";
 import AdminPage from "./pages/AdminPage";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ValidatePage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/validate" element={<ValidatePage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </Router>
